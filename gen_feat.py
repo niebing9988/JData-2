a843cae563851108bc622f57b014341ed29cb746
edvard <edvard_hua@live.com>
2017-05-15 13:06:32 +0800

    fix bug

--- gen_feat.py
+++ gen_feat.py
@@ -9,18 +9,13 @@ import os
 import math
 import numpy as np
 
-# action_1_path = "./data/JData_Action_201602.csv"
-# action_2_path = "./data/JData_Action_201603.csv"
-# action_3_path = "./data/JData_Action_201604.csv"
-# user_path = "./data/JData_User.csv"
+action_1_path = "./data/actions2.csv"
+action_2_path = "./data/actions3.csv"
+action_3_path = "./data/actions4.csv"
+user_path = "./data/add_cart_11_users.csv"
 
-action_1_path = "./data/sample/two_user_actions2.csv"
-action_2_path = "./data/sample/two_user_actions3.csv"
-action_3_path = "./data/sample/two_user_actions4.csv"
-user_path = "./data/sample/two_user.csv"
-
-comment_path = "./data/JData_Comment.csv"
-product_path = "./data/JData_Product.csv"
+comment_path = "./data/comment.csv"
+product_path = "./data/product.csv"
 
 
 comment_date = ["2016-02-01", "2016-02-08",
@@ -31,24 +26,29 @@ comment_date = ["2016-02-01", "2016-02-08",
 
 
 def convert_age(age_str):
-    if age_str == u'-1':
+    if age_str == '-1':
         return 0
-    elif age_str == u'15岁以下':
+    elif age_str == '15岁以下':
         return 1
-    elif age_str == u'16-25岁':
+    elif age_str == '16-25岁':
         return 2
-    elif age_str == u'26-35岁':
+    elif age_str == '26-35岁':
         return 3
-    elif age_str == u'36-45岁':
+    elif age_str == '36-45岁':
         return 4
-    elif age_str == u'46-55岁':
+    elif age_str == '46-55岁':
         return 5
-    elif age_str == u'56岁以上':
+    elif age_str == '56岁以上':
         return 6
     else:
         return -1
 
 def get_basic_user_feat():
+    """
+    这里只是简单将用户表类型进行转换
+    :return:
+    """
+
     dump_path = './cache/basic_user.pkl'
     if os.path.exists(dump_path):
         user = pickle.load(open(dump_path))
@@ -59,7 +59,6 @@ def get_basic_user_feat():
         user['age'] = user['age'].map(convert_age)
         age_df = pd.get_dummies(user["age"], prefix="age")
         sex_df = pd.get_dummies(user["sex"], prefix="sex")
-        # TODO 这个类别处理有问题，是有大小区别
         user_lv_df = pd.get_dummies(user["user_lv_cd"], prefix="user_lv_cd")
 
         user = pd.concat([user['user_id'], age_df, sex_df, user_lv_df], axis=1)
@@ -108,7 +107,7 @@ def get_actions(start_date, end_date):
         action_1 = get_actions_1()
         action_2 = get_actions_2()
         action_3 = get_actions_3()
-        actions = pd.concat([action_1, action_2, action_3]) # type: pd.DataFrame
+        actions = pd.concat([action_1, action_2, action_3])
         actions = actions[(actions.time >= start_date) & (actions.time < end_date)]
         pickle.dump(actions, open(dump_path, 'w'))
     return actions
@@ -121,8 +120,14 @@ def get_action_feat(start_date, end_date):
     else:
         actions = get_actions(start_date, end_date)
         actions = actions[['user_id', 'sku_id', 'type']]
+
         df = pd.get_dummies(actions['type'], prefix='%s-%s-action' % (start_date, end_date))
-        actions = pd.concat([actions, df], axis=1)  # type: pd.DataFrame
+        acLen = df.shape[0]
+        for i in range(1, 7):
+            if '%s-%s-action' % (start_date, end_date) + str(i) not in df:
+                df['%s-%s-action' % (start_date, end_date) + str(i)] = np.zeros(acLen)
+
+        actions = pd.concat([actions, df], axis=1)
         actions = actions.groupby(['user_id', 'sku_id'], as_index=False).sum()
         del actions['type']
         pickle.dump(actions, open(dump_path, 'w'))
@@ -211,8 +216,12 @@ def get_accumulate_user_feat(start_date, end_date):
         actions = pd.concat([actions['user_id'], df], axis=1)
         actions = actions.groupby(['user_id'], as_index=False).sum()
 
-        # TODO 这里有bug，用户没下单，那么 action_4 将会等于0，那么如何来处理这个值
-        # action 4 是下单的情况
+        # 解决小样本测试出现的bug
+        acLen = actions.shape[0]
+        for i in range(1, 7):
+            if "action_" + str(i) not in actions:
+                actions["action_" + str(i)] = np.zeros(acLen)
+        
         actions['user_action_1_ratio'] = actions['action_4'] / actions['action_1']
         actions['user_action_2_ratio'] = actions['action_4'] / actions['action_2']
         actions['user_action_3_ratio'] = actions['action_4'] / actions['action_3']
@@ -223,16 +232,12 @@ def get_accumulate_user_feat(start_date, end_date):
     return actions
 
 def get_accumulate_product_feat(start_date, end_date):
-
     """
-
     累加计算给定时间段内，不同商品下 下单数/操作数 的占比
-
     :param start_date:
     :param end_date:
     :return:
     """
-
     feature = ['sku_id', 'product_action_1_ratio', 'product_action_2_ratio', 'product_action_3_ratio',
                'product_action_5_ratio', 'product_action_6_ratio']
     dump_path = './cache/product_feat_accumulate_%s_%s.pkl' % (start_date, end_date)
@@ -243,6 +248,13 @@ def get_accumulate_product_feat(start_date, end_date):
         df = pd.get_dummies(actions['type'], prefix='action')
         actions = pd.concat([actions['sku_id'], df], axis=1)
         actions = actions.groupby(['sku_id'], as_index=False).sum()
+        
+        # 解决小样本测试出现的bug
+        acLen = actions.shape[0]
+        for i in range(1, 7):
+            if "action_" + str(i) not in actions:
+                actions["action_" + str(i)] = np.zeros(acLen)
+        
         actions['product_action_1_ratio'] = actions['action_4'] / actions['action_1']
         actions['product_action_2_ratio'] = actions['action_4'] / actions['action_2']
         actions['product_action_3_ratio'] = actions['action_4'] / actions['action_3']
@@ -253,6 +265,12 @@ def get_accumulate_product_feat(start_date, end_date):
     return actions
 
 def get_labels(start_date, end_date):
+    """
+    给定时间内用户是否有下单购买商品
+    :param start_date:
+    :param end_date:
+    :return:
+    """
     dump_path = './cache/labels_%s_%s.pkl' % (start_date, end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
@@ -265,7 +283,6 @@ def get_labels(start_date, end_date):
         pickle.dump(actions, open(dump_path, 'w'))
     return actions
 
-
 def make_test_set(train_start_date, train_end_date):
     dump_path = './cache/test_set_%s_%s.pkl' % (train_start_date, train_end_date)
     if os.path.exists(dump_path):
@@ -275,6 +292,7 @@ def make_test_set(train_start_date, train_end_date):
         user = get_basic_user_feat()
         product = get_basic_product_feat()
         user_acc = get_accumulate_user_feat(start_days, train_end_date)
+
         product_acc = get_accumulate_product_feat(start_days, train_end_date)
         comment_acc = get_comments_product_feat(train_start_date, train_end_date)
         #labels = get_labels(test_start_date, test_end_date)
@@ -282,7 +300,7 @@ def make_test_set(train_start_date, train_end_date):
         # generate 时间窗口
         # actions = get_accumulate_action_feat(train_start_date, train_end_date)
         actions = None
-        for i in (1, 2, 3, 5, 7, 10, 15, 21, 30):
+        for i in (1, 2, 3, 5, 7, 11, 15):
             start_days = datetime.strptime(train_end_date, '%Y-%m-%d') - timedelta(days=i)
             start_days = start_days.strftime('%Y-%m-%d')
             if actions is None:
@@ -298,37 +316,83 @@ def make_test_set(train_start_date, train_end_date):
         actions = pd.merge(actions, comment_acc, how='left', on='sku_id')
         #actions = pd.merge(actions, labels, how='left', on=['user_id', 'sku_id'])
         actions = actions.fillna(0)
-        actions = actions[actions['cate'] == 8]
+        # actions = actions[actions['cate'] == 8]
 
     users = actions[['user_id', 'sku_id']].copy()
     del actions['user_id']
     del actions['sku_id']
+    del actions['cate']
     return users, actions
 
-def make_train_set(train_start_date, train_end_date, test_start_date, test_end_date, days=30):
+def make_train_set(train_start_date, train_end_date, test_start_date, test_end_date):
     dump_path = './cache/train_set_%s_%s_%s_%s.pkl' % (train_start_date, train_end_date, test_start_date, test_end_date)
     if os.path.exists(dump_path):
         actions = pickle.load(open(dump_path))
     else:
         start_days = "2016-02-01"
+        # ['user_id',
+        #  'age_0',
+        #  'age_2',
+        #  'age_3',
+        #  'age_4',
+        #  'age_5',
+        #  'age_6',
+        #  'sex_0',
+        #  'sex_1',
+        #  'sex_2',
+        #  'user_lv_cd_1',
+        #  'user_lv_cd_2',
+        #  'user_lv_cd_3',
+        #  'user_lv_cd_4',
+        #  'user_lv_cd_5']         
         user = get_basic_user_feat()
+        # ['sku_id',
+        #  'cate',
+        #  'brand',
+        #  'a1_-1',
+        #  'a1_1',
+        #  'a1_2',
+        #  'a1_3',
+        #  'a2_-1',
+        #  'a2_1',
+        #  'a2_2',
+        #  'a3_-1',
+        #  'a3_1',
+        #  'a3_2']        
         product = get_basic_product_feat()
-        #累加计算给定时间段内，不同用户下的下单数与其他类型操作数的比率
+        # 累加计算给定时间段内，不同用户下的下单数与其他类型操作数的比率
+        # ['user_id',
+        #  'user_action_1_ratio',
+        #  'user_action_2_ratio',
+        #  'user_action_3_ratio',
+        #  'user_action_5_ratio',
+        #  'user_action_6_ratio']
         user_acc = get_accumulate_user_feat(start_days, train_end_date)
         # 累加计算给定时间段内，不同商品下 下单数/操作数 的占比
+        # ['sku_id',
+        #  'product_action_1_ratio',
+        #  'product_action_2_ratio',
+        #  'product_action_3_ratio',
+        #  'product_action_5_ratio',
+        #  'product_action_6_ratio']        
         product_acc = get_accumulate_product_feat(start_days, train_end_date)
         # 计算规定时间内的，对应商品下不同类型的评论总数和
+        # ['sku_id',
+        #  'has_bad_comment',
+        #  'bad_comment_rate',
+        #  'comment_num_1',
+        #  'comment_num_2',
+        #  'comment_num_3',
+        #  'comment_num_4']        
         comment_acc = get_comments_product_feat(train_start_date, train_end_date)
-        # 在这段时间内，用户是否有下单买过某些商品
+        # 在测试的段时间内，用户是否有下单买过某些商品
+        # ['user_id', 'sku_id', 'label']
         labels = get_labels(test_start_date, test_end_date)
-
-        # 强制退出，用户调试
-        exit()
 
         # generate 时间窗口
         # actions = get_accumulate_action_feat(train_start_date, train_end_date)
         actions = None
-        for i in (1, 2, 3, 5, 7, 10, 15, 21, 30):
+        for i in (1, 2, 3, 5, 7, 11, 15):
             start_days = datetime.strptime(train_end_date, '%Y-%m-%d') - timedelta(days=i)
             start_days = start_days.strftime('%Y-%m-%d')
             if actions is None:
@@ -347,63 +411,95 @@ def make_train_set(train_start_date, train_end_date, test_start_date, test_end_d
 
     users = actions[['user_id', 'sku_id']].copy()
     labels = actions['label'].copy()
+    # 删除掉无用的字段
     del actions['user_id']
     del actions['sku_id']
     del actions['label']
+    del actions['cate']
 
+    # ['2016-02-29-2016-03-01-action_1',
+    #  '2016-02-29-2016-03-01-action_6',
+    #  '2016-02-28-2016-03-01-action_1',
+    #  '2016-02-28-2016-03-01-action_6',
+    #  '2016-02-27-2016-03-01-action_1',
+    #  '2016-02-27-2016-03-01-action_6',
+    #  '2016-02-25-2016-03-01-action_1',
+    #  '2016-02-25-2016-03-01-action_6',
+    #  '2016-02-23-2016-03-01-action_1',
+    #  '2016-02-23-2016-03-01-action_6',
+    #  '2016-02-19-2016-03-01-action_1',
+    #  '2016-02-19-2016-03-01-action_2',
+    #  '2016-02-19-2016-03-01-action_4',
+    #  '2016-02-19-2016-03-01-action_6',
+    #  '2016-02-15-2016-03-01-action_1',
+    #  '2016-02-15-2016-03-01-action_2',
+    #  '2016-02-15-2016-03-01-action_4',
+    #  '2016-02-15-2016-03-01-action_6',
+    #  '2016-02-09-2016-03-01-action_1',
+    #  '2016-02-09-2016-03-01-action_2',
+    #  '2016-02-09-2016-03-01-action_4',
+    #  '2016-02-09-2016-03-01-action_6',
+    #  '2016-01-31-2016-03-01-action_1',
+    #  '2016-01-31-2016-03-01-action_2',
+    #  '2016-01-31-2016-03-01-action_4',
+    #  '2016-01-31-2016-03-01-action_6',
+    #  'age_0',
+    #  'age_2',
+    #  'age_3',
+    #  'age_4',
+    #  'age_5',
+    #  'age_6',
+    #  'sex_0',
+    #  'sex_1',
+    #  'sex_2',
+    #  'user_lv_cd_1',
+    #  'user_lv_cd_2',
+    #  'user_lv_cd_3',
+    #  'user_lv_cd_4',
+    #  'user_lv_cd_5',
+    #  'user_action_1_ratio',
+    #  'user_action_2_ratio',
+    #  'user_action_3_ratio',
+    #  'user_action_5_ratio',
+    #  'user_action_6_ratio',
+    #  'cate',
+    #  'brand',
+    #  'a1_-1',
+    #  'a1_1',
+    #  'a1_2',
+    #  'a1_3',
+    #  'a2_-1',
+    #  'a2_1',
+    #  'a2_2',
+    #  'a3_-1',
+    #  'a3_1',
+    #  'a3_2',
+    #  'product_action_1_ratio',
+    #  'product_action_2_ratio',
+    #  'product_action_3_ratio',
+    #  'product_action_5_ratio',
+    #  'product_action_6_ratio',
+    #  'has_bad_comment',
+    #  'bad_comment_rate',
+    #  'comment_num_1',
+    #  'comment_num_2',
+    #  'comment_num_3',
+    #  'comment_num_4']
     return users, actions, labels
 
-
-def report(pred, label):
-
-    actions = label
-    result = pred
-
-    # 所有用户商品对
-    all_user_item_pair = actions['user_id'].map(str) + '-' + actions['sku_id'].map(str)
-    all_user_item_pair = np.array(all_user_item_pair)
-    # 所有购买用户
-    all_user_set = actions['user_id'].unique()
-
-    # 所有品类中预测购买的用户
-    all_user_test_set = result['user_id'].unique()
-    all_user_test_item_pair = result['user_id'].map(str) + '-' + result['sku_id'].map(str)
-    all_user_test_item_pair = np.array(all_user_test_item_pair)
-
-    # 计算所有用户购买评价指标
-    pos, neg = 0,0
-    for user_id in all_user_test_set:
-        if user_id in all_user_set:
-            pos += 1
-        else:
-            neg += 1
-    all_user_acc = 1.0 * pos / ( pos + neg)
-    all_user_recall = 1.0 * pos / len(all_user_set)
-    print '所有用户中预测购买用户的准确率为 ' + str(all_user_acc)
-    print '所有用户中预测购买用户的召回率' + str(all_user_recall)
-
-    pos, neg = 0, 0
-    for user_item_pair in all_user_test_item_pair:
-        if user_item_pair in all_user_item_pair:
-            pos += 1
-        else:
-            neg += 1
-    all_item_acc = 1.0 * pos / ( pos + neg)
-    all_item_recall = 1.0 * pos / len(all_user_item_pair)
-    print '所有用户中预测购买商品的准确率为 ' + str(all_item_acc)
-    print '所有用户中预测购买商品的召回率' + str(all_item_recall)
-    F11 = 6.0 * all_user_recall * all_user_acc / (5.0 * all_user_recall + all_user_acc)
-    F12 = 5.0 * all_item_acc * all_item_recall / (2.0 * all_item_recall + 3 * all_item_acc)
-    score = 0.4 * F11 + 0.6 * F12
-    print 'F11=' + str(F11)
-    print 'F12=' + str(F12)
-    print 'score=' + str(score)
-
 if __name__ == '__main__':
-    train_start_date = '2016-02-01'
-    train_end_date = '2016-03-01'
-    test_start_date = '2016-03-01'
-    test_end_date = '2016-03-05'
-    user, action, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
-    print user.head(10)
-    print action.head(10)
+    get_action_feat("2016-02-01", "2016-02-05")
+
+    # train_start_date = '2016-02-01'
+    # train_end_date = '2016-03-01'
+    # test_start_date = '2016-03-01'
+    # test_end_date = '2016-03-05'
+
+    # user, action, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
+    
+    # print user.head(10)
+    # print action.head(10)
+    # print label.head(10)
+
+
+
--- train.py
+++ train.py
@@ -5,7 +5,9 @@ from gen_feat import make_train_set
 from gen_feat import make_test_set
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
-from gen_feat import report
+from local_evaluation import report
+from scipy import stats
+import matplotlib.pyplot as plt
 
 
 def xgboost_make_submission():
@@ -14,7 +16,7 @@ def xgboost_make_submission():
     test_start_date = '2016-04-11'
     test_end_date = '2016-04-16'
 
-    sub_start_date = '2016-03-15'
+    sub_start_date = '2016-04-05'
     sub_end_date = '2016-04-16'
 
     user_index, training_data, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
@@ -31,6 +33,7 @@ def xgboost_make_submission():
     #param['eval_metric'] = "auc"
     # 转换成元组
     plst = param.items()
+    # 设置模型的评价指标
     plst += [('eval_metric', 'logloss')]
     evallist = [(dtest, 'eval'), (dtrain, 'train')]
     bst=xgb.train(plst, dtrain, num_round, evallist)
@@ -49,43 +52,52 @@ def xgboost_make_submission():
 
 
 def xgboost_cv():
-    train_start_date = '2016-03-05'
-    train_end_date = '2016-04-06'
-    test_start_date = '2016-04-11'
-    test_end_date = '2016-04-16'
+    # 一个月为训练数据
+    train_start_date = '2016-02-15'
+    train_end_date = '2016-03-15'
+    # 根据上一个月训练的数据，预测未来五天的数据
+    test_start_date = '2016-03-16'
+    test_end_date = '2016-03-20'
 
-    sub_start_date = '2016-02-05'
-    sub_end_date = '2016-03-05'
-    sub_test_start_date = '2016-03-05'
-    sub_test_end_date = '2016-03-10'
+    # 用之前的数据验证模型的有效性
+    # 输入
+    sub_start_date = '2016-03-21'
+    sub_end_date = '2016-04-02'
+    # 输出
+    sub_test_start_date = '2016-04-03'
+    sub_test_end_date = '2016-04-08'
 
     user_index, training_data, label = make_train_set(train_start_date, train_end_date, test_start_date, test_end_date)
     # 简单划分 训练集和验证集
     X_train, X_test, y_train, y_test = train_test_split(training_data, label, test_size=0.2, random_state=0)
-    dtrain=xgb.DMatrix(X_train, label=y_train)
-    dtest=xgb.DMatrix(X_test, label=y_test)
-    param = {'learning_rate' : 0.1, 'n_estimators': 1000, 'max_depth': 3,
-        'min_child_weight': 5, 'gamma': 0, 'subsample': 1.0, 'colsample_bytree': 0.8,
-        'scale_pos_weight': 1, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
-    num_round = 250
+    dtrain=xgb.DMatrix(X_train.values, label=y_train)
+    dtest=xgb.DMatrix(X_test.values, label=y_test)
+    param = {'max_depth': 10, 'eta': 0.05, 'silent': 1, 'objective': 'binary:logistic'}
+    num_round = 166
     param['nthread'] = 5
     param['eval_metric'] = "auc"
     plst = param.items()
-    plst += [('eval_metric', 'logloss')]
     evallist = [(dtest, 'eval'), (dtrain, 'train')]
     bst=xgb.train(plst, dtrain, num_round, evallist)
 
-    sub_user_index, sub_trainning_date, sub_label = make_train_set(sub_start_date, sub_end_date,
+    sub_user_index, sub_trainning_data, sub_label = make_train_set(sub_start_date, sub_end_date,
                                                                    sub_test_start_date, sub_test_end_date)
-    test = xgb.DMatrix(sub_trainning_date)
-    y = bst.predict(test)
+    sub_trainning_data = xgb.DMatrix(sub_trainning_data.values)
+    y = bst.predict(sub_trainning_data)
+
+    y_mean = stats.describe(y).mean
+    # plt.hist(y)
+    # plt.show()
 
     pred = sub_user_index.copy()
     y_true = sub_user_index.copy()
     pred['label'] = y
     y_true['label'] = label
-    report(pred, y_true)
 
+    pred = pred[pred['label'] >= 0.04]
+    y_true = y_true[y_true['label'] == 1]
+
+    report(pred, y_true)
 
 if __name__ == '__main__':
     xgboost_cv()
